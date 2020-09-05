35813e31dbe549baedc9df07e14801052f6681ba
Amila Welihinda <amilajack@gmail.com>
2020-09-05 14:58:38 -0700

    fix failing build

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
-import "@testing-library/jest-dom";
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
 import App from "../App";
 
 describe("App", () => {
