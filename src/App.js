ae710f2d096e6853314d8f7650c6edc32544b157
Hamir Mahal <hamirmahal@gmail.com>
2020-10-31 14:59:00 -0700

    stop waveform from being visible through open react-modal window

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sound from "./Sound";
 import Search from "./Search";
 import auth, { init } from "./Auth";
 // Bind modal to our appElement for accessibility
-Modal.setAppElement(document.getElementById('root'));
+Modal.setAppElement(document.getElementById("root"));
 
 const freeSound = new FreeSound();
 
@@ -62,20 +62,24 @@ const App = () => {
         <ion-icon name="search-sharp"></ion-icon>
       </div>
       <Modal
-        className="bg-primary color-secondary max-w-lg mx-auto my-32 p-8"
+        className="bg-primary border-2 color-secondary max-w-lg mx-auto my-32 p-8"
         contentLabel="Window confirming visit to Freesound"
         isOpen={modalIsOpen}
         onRequestClose={closeModal} // Really important!
-        // The above line appears to be what allows users 
+        // The above line appears to be what allows users
         // to exit the Modal by simply hitting the Esc key
         // or clicking anywhere else on the page.
         // Please keep onRequestClose={closeModal} in here
         // in future iterations for
         // an improved user experience.
+        overlayClassName="fixed inset-0 z-10"
       >
         <h1>Redirecting to Freesound</h1> <br />
+        <button className="absolute right-0 top-0" onClick={closeModal}>
+          {<ion-icon name="close-sharp"></ion-icon>}
+        </button>
         <p>
-          After logging in with Freesound,
+          After logging in with Freesound, {/* eslint-disable-next-line */}
           you'll be sent back here!
         </p>
         <br />
@@ -86,10 +90,7 @@ const App = () => {
           >
             Send me there!
           </button>
-          <button 
-            className="border rounded px-4 py-1"
-            onClick={closeModal}
-          >
+          <button className="border rounded px-4 py-1" onClick={closeModal}>
             Maybe Later
           </button>
         </span>
