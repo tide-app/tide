082afa6998928acd44107e0fd9957445152f99ce
Hamir Mahal <hamirmahal@gmail.com>
2020-11-01 10:47:12 -0800

    make background transparent and make modal window always have gold border

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [],
   theme: {
     colors: {
+      gold: "#FBDC57", // if we always want gold
       primary: "var(--primary-color)",
       secondary: "var(--secondary-color)",
     },
