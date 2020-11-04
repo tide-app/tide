6ca5470bdb15a5a95f13b410755e949d5cd3793c
Hamir Mahal <hamirmahal@gmail.com>
2020-11-03 19:38:10 -0800

    revert change to tailwind.config.js and add changes to modal window

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ module.exports = {
   purge: [],
   theme: {
     colors: {
-      gold: "#FBDC57", // if we always want gold
       primary: "var(--primary-color)",
       secondary: "var(--secondary-color)",
     },
