9388adba854699c63a9b3eb751eb3bb2364e65e7
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2020-11-04 11:06:57 -0800

    minor styling fixes

--- src/SoundList.js
+++ src/SoundList.js
@@ -4,26 +4,33 @@ import { Link } from "react-router-dom";
 const SoundList = ({ tracks, header, selectedTrack, onSoundClick }) => {
   return (
     <div className="text-left px-10 pt-16">
-      {header && (
-        <h1 className="text-3xl m-auto border-b border-bg-default">{header}</h1>
-      )}
-      {tracks.map((track) => (
-        <Link
-          key={track.id}
-          to={`/sound/${track.id}`}
-          onClick={() => (onSoundClick ? onSoundClick(track) : () => {})}
-        >
-          <div
-            className={
-              track.id === selectedTrack?.id
-                ? "playlist-item selected"
-                : "playlist-item"
-            }
+      {header && <h1 className="text-3xl m-auto">{header}</h1>}
+      <div className="list-item-container">
+        {tracks.map((track) => (
+          <Link
+            key={track.id}
+            to={`/sound/${track.id}`}
+            onClick={() => (onSoundClick ? onSoundClick(track) : () => {})}
           >
-            {track.name}
-          </div>
-        </Link>
-      ))}
+            <div
+              className={
+                track.id === selectedTrack?.id
+                  ? "list-item selected"
+                  : "list-item"
+              }
+            >
+              <div className="">
+                <ion-icon
+                  className="inline-flex"
+                  id="play-button"
+                  name="play-circle-sharp"
+                />
+                {track.name}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
