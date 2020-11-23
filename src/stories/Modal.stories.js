1dbd443492bd397545b86f34f91d3b607fecd9b7
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2020-11-22 22:27:58 -0800

    Storybook component for modal in the works

---
+++ src/stories/Modal.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../build.css";
+
+import Modal from "react-modal";
+
+export default {
+  title: "Example/Modal",
+  component: Modal,
+};
+
+const Template = (args) => <Modal {...args}>Hello World!</Modal>;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  primary: true,
+  label: "Modal",
+};
