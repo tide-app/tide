423341384984852debe840480f522b824c3059f9
Hamir Mahal <hamirmahal@gmail.com>
2020-11-28 16:28:24 -0800

    add e2e tests checking search results and replaced id

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,7 +5,6 @@ export default function Input({ ...props }) {
     <input
       {...props}
       className="appearance-none block placeholder-secondary text-secondary bg-primary border rounded p-1 leading-tight focus:outline-none pl-4"
-      data-e2e-id="search-input"
     />
   );
 }
