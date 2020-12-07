bbe238de1b2ffde17f797638f6eaccb2a8279361
Hamir Mahal <hamirmahal@gmail.com>
2020-12-07 11:16:14 -0800

    resize search bar and fix search icon's alignment

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,10 +6,13 @@ export default function Input({ icon, ...props }) {
     <div className="relative">
       <input
         {...props} // appearance-none, block and leading-tight don't seem to do anything. Should we remove them?
-        className="appearance-none block bg-primary border focus:outline-none h-10 leading-tight p-1 pl-4 placeholder-secondary rounded"
+        className="appearance-none block bg-primary border focus:outline-none leading-tight p-1 pl-4 placeholder-secondary rounded"
       />
       {icon && (
-        <button type="submit" className="absolute mr-4 mt-3 right-0 top-0">
+        <button
+          type="submit"
+          className="absolute flex h-full items-center mr-4 right-0 top-0"
+        >
           <IonIcon icon={icon} />
         </button>
       )}
