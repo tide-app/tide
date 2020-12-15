790b429c64726163d7a035149432c2307a75efa1
hamirmahal <hamirmahal@gmail.com>
2020-12-14 21:41:42 -0800

    Make tag styles change on hover and clickable
    Clicking on tags shows a tag page, and tag styles will change on hover
    reflecting their clickability

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Tags({ tags = [] }) {
   return (
     <>
-      <div className="py-4 text-left mb-4 opacity-50">
+      <div className="py-4 text-left mb-4">
         {tags.map((tag) => (
-          <div
-            key={tag}
-            className="inline-block px-3 rounded mr-4 mb-4 border border-solid border-secondary"
-          >
-            {tag}
-          </div>
+          <Link data-e2e-id="SoundList-track-url" key={tag} to={`/tag/${tag}`}>
+            <div
+              key={tag}
+              className="hover:bg-secondary hover:text-primary hover:opacity-100 opacity-50 transition duration-150 ease-in-out inline-block px-3 rounded mr-4 mb-4 border border-solid border-secondary"
+            >
+              {tag}
+            </div>
+          </Link>
         ))}
       </div>
     </>
