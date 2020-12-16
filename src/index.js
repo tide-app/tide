dbca2791f90466fe975dae3a270573896932633a
Amila Welihinda <amilajack@gmail.com>
2020-12-15 19:25:12 -0800

    infra: add sentry integration (#284)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import { register } from "./serviceWorker";
 import "./build.css";
+
+Sentry.init({
+  dsn:
+    "https://db77445a37ad4dcbaa33ca7f33dec571@o184269.ingest.sentry.io/5558822",
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 0.1,
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
