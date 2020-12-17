03da2b478bf96076a47f75e15bd1412eb2f9386e
Hamir Mahal <hamirmahal@gmail.com>
2020-12-17 11:34:10 -0800

    Lay groundwork for pagination to close #169

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { SOUND_LIST_QUERY_PARAMS } from "./constants";
 import SoundListContainer from "./SoundListContainer";
+import Paginate from "./components/Paginate.tsx";
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
@@ -14,14 +15,29 @@ export default function Search({
   fetchSearchResults,
   showHeader = false,
 }) {
+  // eslint-disable-next-line camelcase
+  const page_size = 5;
+  const [
+    searchOptionsForSearchResults,
+    setSearchOptionsForSearchResults,
+  ] = useState({
+    ...SOUND_LIST_QUERY_PARAMS,
+    page_size,
+  });
   const [searchResults, setSearchResults] = useState([]);
+  const [totalNumberOfSearchResults, setTotalNumberOfSearchResults] = useState(
+    0
+  );
   const queryParams = useQuery();
   const queryValue = queryParams.get("q");
   const searchQuery = typeof queryValue === "string" ? queryValue : undefined;
 
   const debouncedSearch = useDebouncedCallback(
     () =>
-      fetchSearchResults(searchQuery || searchValue, SOUND_LIST_QUERY_PARAMS),
+      fetchSearchResults(
+        searchQuery || searchValue,
+        searchOptionsForSearchResults
+      ),
     300,
     {
       leading: true,
@@ -36,13 +52,14 @@ export default function Search({
         const query = await debouncedSearch.callback();
         if (query?.results) {
           setSearchResults(query.results);
+          setTotalNumberOfSearchResults(query.count);
         }
       }
     };
     if (searchQuery || searchValue) {
       updateSearchResults();
     }
-  }, [searchValue, searchQuery]);
+  }, [searchValue, searchOptionsForSearchResults, searchQuery]);
 
   return (
     <>
@@ -52,13 +69,28 @@ export default function Search({
           <meta name="description" content="Search results from Tide" />
         </Helmet>
       )}
-      <SoundListContainer
-        header={
-          showHeader && `Search results for "${searchQuery || searchValue}"`
+      <Paginate
+        // eslint-disable-next-line camelcase
+        limit={page_size}
+        onPageClick={(page) =>
+          setSearchOptionsForSearchResults((previousSearchOptions) => {
+            return {
+              ...previousSearchOptions,
+              page,
+            };
+          })
         }
-        tracks={searchResults}
-        className="py-16"
-      />
+        page={1}
+        totalResults={totalNumberOfSearchResults}
+      >
+        <SoundListContainer
+          header={
+            showHeader && `Search results for "${searchQuery || searchValue}"`
+          }
+          tracks={searchResults}
+          className="py-16"
+        />
+      </Paginate>
     </>
   );
 }
---
+++ src/components/Paginate.tsx
@@ -0,0 +1,200 @@
+import React, { useEffect, useState } from "react";
+
+const findNumberOfPages = (
+  numberOfResults: number,
+  numberOfResultsPerPage: number
+): number => Math.ceil(numberOfResults / numberOfResultsPerPage);
+
+interface paginateProps {
+  children: any; // children: component?
+  limit: number;
+  onPageClick: Function;
+  page: number;
+  totalResults: number;
+}
+
+export default function Paginate({
+  children,
+  limit,
+  onPageClick,
+  page,
+  totalResults,
+}: paginateProps) {
+  const [currentPage, setCurrentPage] = useState(page);
+  const numberOfPages: number = findNumberOfPages(totalResults, limit);
+
+  useEffect(() => {
+    onPageClick(currentPage);
+  }, [currentPage]);
+
+  return (
+    <>
+      {children}
+      {/* <!-- This example requires Tailwind CSS v2.0+ --> */}
+      <div className="bg-primary px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+        <div className="flex-1 flex justify-between sm:hidden">
+          {currentPage !== 1 && (
+            <button
+              className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-primary hover:text-gray-500"
+              onClick={() => setCurrentPage(currentPage - 1)}
+            >
+              Previous
+            </button>
+          )}
+          {currentPage !== numberOfPages && (
+            <button
+              className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-primary hover:text-gray-500"
+              onClick={() => setCurrentPage(currentPage + 1)}
+            >
+              Next
+            </button>
+          )}
+        </div>
+        <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+          <div>
+            <p className="text-sm text-gray-700">
+              Showing
+              <span className="font-medium">
+                {" "}
+                {currentPage * limit - (limit - 1)}{" "}
+              </span>
+              to
+              <span className="font-medium">
+                {" " /* Are we on the last page, and... */}
+                {currentPage === numberOfPages && totalResults % limit !== 0
+                  ? (totalResults % limit) - // does the number of results
+                    1 + // per page divide the total number of results evenly?
+                    currentPage * limit -
+                    (limit - 1)
+                  : currentPage * limit}{" "}
+              </span>
+              of
+              <span className="font-medium"> {totalResults} </span>
+              results
+            </p>
+          </div>
+          <div>
+            <nav
+              className="relative z-0 inline-flex shadow-sm -space-x-px"
+              aria-label="Pagination"
+            >
+              {currentPage !== 1 && (
+                <button // Only show the previous page button if we're not on the first page.
+                  className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-primary text-sm font-medium text-gray-500 hover:bg-secondary hover:text-primary transition duration-150 ease-in-out"
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                >
+                  <span className="sr-only">Previous</span>
+                  {/* <!-- Heroicon name: chevron-left --> */}
+                  <svg
+                    className="h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              )}
+              <button
+                onClick={() => setCurrentPage(1)}
+                className={`border duration-150 ease-in-out font-medium hover:bg-secondary hover:text-primary inline-flex items-center px-4 py-2 relative text-sm transition ${
+                  currentPage === 1 // Are we on the first page? If so, make the lefthand size of
+                    ? "bg-secondary rounded-l-md text-primary" // the first page's button rounded.
+                    : "bg-primary text-secondary"
+                }`}
+              >
+                1
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentPage(2);
+                }}
+                className={`border duration-150 ease-in-out font-medium hover:bg-secondary hover:text-primary inline-flex items-center px-4 py-2 relative text-sm transition ${
+                  currentPage === 2
+                    ? "bg-secondary text-primary"
+                    : "bg-primary text-secondary"
+                }`}
+              >
+                2
+              </button>
+              <button
+                onClick={() => setCurrentPage(3)}
+                className={`border duration-150 ease-in-out font-medium hover:bg-secondary hover:text-primary inline-flex items-center px-4 py-2 relative text-sm transition ${
+                  currentPage === 3
+                    ? "bg-secondary text-primary"
+                    : "bg-primary text-secondary"
+                }`}
+              >
+                3
+              </button>
+              <span
+                className={
+                  "border duration-150 ease-in-out font-medium hover:bg-secondary hover:text-primary inline-flex items-center px-4 py-2 relative text-sm transition bg-primary text-secondary"
+                }
+              >
+                ...
+              </span>
+              <button
+                onClick={() => setCurrentPage(numberOfPages - 2)}
+                className={`border duration-150 ease-in-out font-medium hover:bg-secondary hover:text-primary inline-flex items-center px-4 py-2 relative text-sm transition ${
+                  currentPage === numberOfPages - 2
+                    ? "bg-secondary text-primary"
+                    : "bg-primary text-secondary"
+                }`}
+              >
+                {numberOfPages - 2}
+              </button>
+              <button
+                onClick={() => setCurrentPage(numberOfPages - 1)}
+                className={`border duration-150 ease-in-out font-medium hover:bg-secondary hover:text-primary inline-flex items-center px-4 py-2 relative text-sm transition ${
+                  currentPage === numberOfPages - 1
+                    ? "bg-secondary text-primary"
+                    : "bg-primary text-secondary"
+                }`}
+              >
+                {numberOfPages - 1}
+              </button>
+              <button
+                onClick={() => setCurrentPage(numberOfPages)}
+                className={`border duration-150 ease-in-out font-medium hover:bg-secondary hover:text-primary inline-flex items-center px-4 py-2 relative text-sm transition ${
+                  currentPage === numberOfPages // Are we on the first page? If so, make the righthand size of
+                    ? "bg-secondary rounded-r-md text-primary" // the last page's button rounded.
+                    : "bg-primary text-secondary"
+                }`}
+              >
+                {numberOfPages}
+              </button>
+              {currentPage !== numberOfPages && (
+                <button // Only show the next page button if we're not on the last page.
+                  className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-primary text-sm font-medium text-gray-500 hover:bg-secondary hover:text-primary transition duration-150 ease-in-out"
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                >
+                  <span className="sr-only">Next</span>
+                  {/* <!-- Heroicon name: chevron-right --> */}
+                  <svg
+                    className="h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              )}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
