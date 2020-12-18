454cd2c9dbe9aa101041eb4062d2157c7b15855a
Hamir Mahal <hamirmahal@gmail.com>
2020-12-18 09:34:19 -0800

    Auto-fix spacing of app_spec.ts; no manual changes
    I did not make any manual changes in this commit. This commit automatically fixes the spacing in `app_spec.ts`. I did this by saving `app_spec.ts` twice. I didn't add anything to the body of the file.

--- cypress/integration/app_spec.ts
+++ cypress/integration/app_spec.ts
@@ -1,70 +1,80 @@
 /* eslint-disable jest/valid-expect, jest/expect-expect */
 function setupServerMocks() {
-  cy.intercept('GET', 'https://freesound.org/apiv2/sounds/462808', {fixture:  'sound.json'})
-  cy.intercept('GET', '/people/qfox123/packs/26158', {fixture: 'sound.json'})
-  cy.intercept('GET', 'https://freesound.org/apiv2/packs/26158/', {fixture:'pack.json'})
-  cy.intercept('GET', 'https://freesound.org/apiv2/packs/26158/sounds/?fields=id%2Cname%2Cduration%2Cnum_downloads%2Cusername%2Cnum_ratings', {fixture:'pack-sounds.json'})
-  cy.intercept('GET', 'https://freesound.org/apiv2/sounds/462808/similar/?fields=id%2Cname%2Cduration%2Cnum_downloads%2Cusername%2Cnum_ratings', {fixture:'similar-sounds.json'})
-  cy.intercept('GET', 'https://freesound.org/data/previews/462/462808_8386274-lq.mp3', {fixture:'sound.mp3'})
-  cy.intercept('GET', 'https://freesound.org/apiv2/search/text/*', {fixture:'search-results.json'})
+  cy.intercept("GET", "https://freesound.org/apiv2/sounds/462808", {
+    fixture: "sound.json",
+  });
+  cy.intercept("GET", "/people/qfox123/packs/26158", { fixture: "sound.json" });
+  cy.intercept("GET", "https://freesound.org/apiv2/packs/26158/", {
+    fixture: "pack.json",
+  });
+  cy.intercept(
+    "GET",
+    "https://freesound.org/apiv2/packs/26158/sounds/?fields=id%2Cname%2Cduration%2Cnum_downloads%2Cusername%2Cnum_ratings",
+    { fixture: "pack-sounds.json" }
+  );
+  cy.intercept(
+    "GET",
+    "https://freesound.org/apiv2/sounds/462808/similar/?fields=id%2Cname%2Cduration%2Cnum_downloads%2Cusername%2Cnum_ratings",
+    { fixture: "similar-sounds.json" }
+  );
+  cy.intercept(
+    "GET",
+    "https://freesound.org/data/previews/462/462808_8386274-lq.mp3",
+    { fixture: "sound.mp3" }
+  );
+  cy.intercept("GET", "https://freesound.org/apiv2/search/text/*", {
+    fixture: "search-results.json",
+  });
 }
 
 describe("Home Page", () => {
   beforeEach(() => {
-    setupServerMocks()
-  })
+    setupServerMocks();
+  });
 
   it("should load the home page", () => {
     cy.visit("/");
-    cy
-      .get(`[data-e2e-id='SoundList']`)
-      .should('be.visible')
+    cy.get(`[data-e2e-id='SoundList']`).should("be.visible");
   });
 });
 
 describe("Search functionality", () => {
   beforeEach(() => {
-    setupServerMocks()
-  })
-
-  it("checks that the returned search results are correct ", () => {
-    cy.visit("/search?q=hello");
-    cy
-      .get(`[data-e2e-id="SoundList"]`)
-      .should('be.visible');
-    cy
-      .get(`[data-e2e-id="SoundList-track-name"]`)
-      .first()
-      .should('be.visible')
-      .should('have.text', 'hello');
-    cy
-      .get(`[data-e2e-id="SoundList-track-url"]`)
-      .first()
-      .should('be.visible')
-      .should('have.attr', 'href', '/sound/123')
+    setupServerMocks();
   });
 
-  it("checks that search updates the url correctly ", () => {
+  it("checks that the returned search results are correct", () => {
+    cy.visit("/search?q=hello");
+    cy.get(`[data-e2e-id="SoundList"]`).should("be.visible");
+    cy.get(`[data-e2e-id="SoundList-track-name"]`)
+      .first()
+      .should("be.visible")
+      .should("have.text", "hello");
+    cy.get(`[data-e2e-id="SoundList-track-url"]`)
+      .first()
+      .should("be.visible")
+      .should("have.attr", "href", "/sound/123");
+  });
+
+  it("checks that search updates the url correctly", () => {
     cy.visit("/");
-    cy
-      .get(`[data-e2e-id="search-input"]`)
-      .type('hello')
-      .should('have.value', 'hello')
-    cy.url().should('include', '/search?q=hello')
+    cy.get(`[data-e2e-id="search-input"]`)
+      .type("hello")
+      .should("have.value", "hello");
+    cy.url().should("include", "/search?q=hello");
   });
 });
 
 describe("Sound Page", () => {
   beforeEach(() => {
-    setupServerMocks()
-  })
+    setupServerMocks();
+  });
 
   it("should load the page", () => {
-    const testSoundId = '462808';
+    const testSoundId = "462808";
     cy.visit(`/sound/${testSoundId}`);
-    cy
-      .get(`[data-e2e-id="sound-title"]`)
-      .should('be.visible')
-      .should('have.text', 'Music note 9')
+    cy.get(`[data-e2e-id="sound-title"]`)
+      .should("be.visible")
+      .should("have.text", "Music note 9");
   });
 });
