6680cbe9984ae1bafe1f028d8bbddde59833104a
Hamir Mahal <hamirmahal@gmail.com>
2020-12-22 17:57:10 -0800

    Only show pagination bar for >= 2 pages of results

--- src/components/Paginate.tsx
+++ src/components/Paginate.tsx
@@ -68,114 +68,116 @@ export default function Paginate({
             <span> {totalResults} </span>
             {totalResults === 1 ? "result" : "results"}
           </p>
-          <nav
-            className="z-0 inline-flex shadow-sm -space-x-px text-sm"
-            aria-label="Pagination"
-          >
-            {currentPage !== 1 && (
-              <button // Only show the previous page button if we're not on the first page.
-                className="inline-flex px-2 py-2 rounded-l-md border hover:bg-secondary hover:border-secondary hover:text-primary transition duration-150 ease-in-out"
-                onClick={() => setCurrentPage(currentPage - 1)}
-              >
-                <span className="sr-only">Previous</span>
-                {/* <!-- Heroicon name: chevron-left --> */}
-                <svg
-                  className="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
+          {numberOfPages > 1 && (
+            <nav
+              className="z-0 inline-flex shadow-sm -space-x-px text-sm"
+              aria-label="Pagination"
+            >
+              {currentPage !== 1 && (
+                <button // Only show the previous page button if we're not on the first page.
+                  className="inline-flex px-2 py-2 rounded-l-md border hover:bg-secondary hover:border-secondary hover:text-primary transition duration-150 ease-in-out"
+                  onClick={() => setCurrentPage(currentPage - 1)}
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            )}
-            <button
-              onClick={() => setCurrentPage(1)}
-              data-e2e-id="first-page-button"
-              className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                currentPage === 1 && // If we're on the first page, make the lefthand side of
-                "bg-secondary border-secondary rounded-l-md text-primary" // the first page's button rounded.
-              }`}
-            >
-              1
-            </button>
-            <button
-              onClick={() => {
-                setCurrentPage(2);
-              }}
-              className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                currentPage === 2 &&
-                "bg-secondary border-secondary text-primary"
-              }`}
-            >
-              2
-            </button>
-            <button
-              onClick={() => setCurrentPage(3)}
-              className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                currentPage === 3 &&
-                "bg-secondary border-secondary text-primary"
-              }`}
-            >
-              3
-            </button>
-            <span className={"border inline-flex px-4 py-2"}>...</span>
-            <button
-              onClick={() => setCurrentPage(numberOfPages - 2)}
-              className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                currentPage === numberOfPages - 2 &&
-                "bg-secondary border-secondary text-primary"
-              }`}
-            >
-              {numberOfPages - 2}
-            </button>
-            <button
-              onClick={() => setCurrentPage(numberOfPages - 1)}
-              className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                currentPage === numberOfPages - 1 &&
-                "bg-secondary border-secondary text-primary"
-              }`}
-            >
-              {numberOfPages - 1}
-            </button>
-            <button
-              onClick={() => setCurrentPage(numberOfPages)}
-              className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                currentPage === numberOfPages && // If we're on the last page, make the righthand side of
-                "bg-secondary border-secondary rounded-r-md text-primary" // the last page's button rounded.
-              }`}
-            >
-              {numberOfPages}
-            </button>
-            {currentPage !== numberOfPages && (
-              <button // Only show the next page button if we're not on the last page.
-                data-e2e-id="next-page-button"
-                className="inline-flex px-2 py-2 rounded-r-md border hover:bg-secondary hover:border-secondary hover:text-primary transition duration-150 ease-in-out"
-                onClick={() => setCurrentPage(currentPage + 1)}
+                  <span className="sr-only">Previous</span>
+                  {/* <!-- Heroicon name: chevron-left --> */}
+                  <svg
+                    className="h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              )}
+              <button
+                onClick={() => setCurrentPage(1)}
+                data-e2e-id="first-page-button"
+                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
+                  currentPage === 1 && // If we're on the first page, make the lefthand side of
+                  "bg-secondary border-secondary rounded-l-md text-primary" // the first page's button rounded.
+                }`}
               >
-                <span className="sr-only">Next</span>
-                {/* <!-- Heroicon name: chevron-right --> */}
-                <svg
-                  className="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                1
               </button>
-            )}
-          </nav>
+              <button
+                onClick={() => {
+                  setCurrentPage(2);
+                }}
+                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
+                  currentPage === 2 &&
+                  "bg-secondary border-secondary text-primary"
+                }`}
+              >
+                2
+              </button>
+              <button
+                onClick={() => setCurrentPage(3)}
+                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
+                  currentPage === 3 &&
+                  "bg-secondary border-secondary text-primary"
+                }`}
+              >
+                3
+              </button>
+              <span className={"border inline-flex px-4 py-2"}>...</span>
+              <button
+                onClick={() => setCurrentPage(numberOfPages - 2)}
+                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
+                  currentPage === numberOfPages - 2 &&
+                  "bg-secondary border-secondary text-primary"
+                }`}
+              >
+                {numberOfPages - 2}
+              </button>
+              <button
+                onClick={() => setCurrentPage(numberOfPages - 1)}
+                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
+                  currentPage === numberOfPages - 1 &&
+                  "bg-secondary border-secondary text-primary"
+                }`}
+              >
+                {numberOfPages - 1}
+              </button>
+              <button
+                onClick={() => setCurrentPage(numberOfPages)}
+                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
+                  currentPage === numberOfPages && // If we're on the last page, make the righthand side of
+                  "bg-secondary border-secondary rounded-r-md text-primary" // the last page's button rounded.
+                }`}
+              >
+                {numberOfPages}
+              </button>
+              {currentPage !== numberOfPages && (
+                <button // Only show the next page button if we're not on the last page.
+                  data-e2e-id="next-page-button"
+                  className="inline-flex px-2 py-2 rounded-r-md border hover:bg-secondary hover:border-secondary hover:text-primary transition duration-150 ease-in-out"
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                >
+                  <span className="sr-only">Next</span>
+                  {/* <!-- Heroicon name: chevron-right --> */}
+                  <svg
+                    className="h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              )}
+            </nav>
+          )}
         </div>
       </div>
     </>
