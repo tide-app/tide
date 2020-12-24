e2560fe6608f5c37930fe82d1f23750382d9edb9
Hamir Mahal <hamirmahal@gmail.com>
2020-12-24 11:52:03 -0800

    Always show current page, even if in a middle page

--- src/components/Paginate.tsx
+++ src/components/Paginate.tsx
@@ -1,3 +1,5 @@
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+/* global JSX */ // Recommended fix for eslint `'JSX' is not defined.
 import { chevronBackSharp, chevronForwardSharp } from "ionicons/icons";
 import { IonIcon } from "@ionic/react";
 import React, { FunctionComponent, useEffect, useState } from "react";
@@ -34,6 +36,73 @@ export default function Paginate({
       lastResultsNumber = firstResultsNumber + leftoverResults - 1;
 
     return ` ${lastResultsNumber.toString()} `;
+  };
+
+  const displayRemainingPageButtons = (): Array<JSX.Element> => {
+    if (numberOfPages < 3) return []; // First/last pages always there
+
+    const buttonForPageNumber = (pageNumber: number): JSX.Element => (
+      <button
+        className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
+          currentPage === pageNumber &&
+          "bg-secondary border-secondary text-primary"
+        }`}
+        onClick={() => setCurrentPage(pageNumber)}
+      >
+        {currentPage === pageNumber ? (
+          <span className="sr-only">You are on page</span>
+        ) : (
+          <span className="sr-only">Jump to page</span>
+        )}
+        {pageNumber}
+      </button>
+    );
+
+    const ELLIPSIS: JSX.Element = (
+      <span className={"border inline-flex px-4 py-2"}>...</span>
+    );
+
+    const MAX_NUMBER_OF_PAGE_BUTTONS: number = 7; // No "magic numbers"!
+    const NUMBER_OF_ALWAYS_PRESENT_PAGE_BUTTONS: number = 3; // First, last
+    const remainingPageButtons: Array<JSX.Element> = []; // and ... buttons
+
+    if (numberOfPages <= MAX_NUMBER_OF_PAGE_BUTTONS) {
+      for (let index = 2; index < numberOfPages; index++)
+        remainingPageButtons.push(buttonForPageNumber(index));
+      // If we're in this case, we can fit every page's button
+      return remainingPageButtons;
+    }
+
+    if (
+      currentPage <= // If we're on one of the first few pages, then...
+      MAX_NUMBER_OF_PAGE_BUTTONS - NUMBER_OF_ALWAYS_PRESENT_PAGE_BUTTONS
+    ) {
+      for (let index = 2; index < MAX_NUMBER_OF_PAGE_BUTTONS - 1; index++)
+        remainingPageButtons.push(buttonForPageNumber(index));
+
+      remainingPageButtons.push(ELLIPSIS);
+      return remainingPageButtons;
+    }
+
+    if (currentPage < numberOfPages - NUMBER_OF_ALWAYS_PRESENT_PAGE_BUTTONS) {
+      remainingPageButtons.push(ELLIPSIS); // If we're in the middle, we show
+      remainingPageButtons.push(buttonForPageNumber(currentPage - 1));
+      remainingPageButtons.push(buttonForPageNumber(currentPage)); // the
+      remainingPageButtons.push(buttonForPageNumber(currentPage + 1));
+      remainingPageButtons.push(ELLIPSIS); // previous, current and next pages
+      // Then, we can exit, because we always show the first and last pages.
+      return remainingPageButtons;
+    }
+
+    remainingPageButtons.push(ELLIPSIS); // If we're on the last few pages,
+    for (
+      let index = numberOfPages - NUMBER_OF_ALWAYS_PRESENT_PAGE_BUTTONS - 1;
+      index < numberOfPages; // We always show the page before the current
+      index++ // one; this is why we substract one from the starting index.
+    )
+      remainingPageButtons.push(buttonForPageNumber(index));
+    // We stop before the last page, because that will always be present.
+    return remainingPageButtons;
   };
 
   useEffect(() => {
@@ -109,73 +178,7 @@ export default function Paginate({
                 )}
                 1
               </button>
-              <button
-                onClick={() => {
-                  setCurrentPage(2);
-                }}
-                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                  currentPage === 2 &&
-                  "bg-secondary border-secondary text-primary"
-                }`}
-              >
-                {currentPage === 2 ? (
-                  <span className="sr-only">You are on page</span>
-                ) : (
-                  <span className="sr-only">Jump to page</span>
-                )}
-                2
-              </button>
-              <button
-                onClick={() => setCurrentPage(3)}
-                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                  currentPage === 3 &&
-                  "bg-secondary border-secondary text-primary"
-                }`}
-              >
-                {currentPage === 3 ? (
-                  <span className="sr-only">You are on page</span>
-                ) : (
-                  <span className="sr-only">Jump to page</span>
-                )}
-                3
-              </button>
-              <span className={"border inline-flex px-4 py-2"}>...</span>
-              <button
-                onClick={() => setCurrentPage(numberOfPages - 2)}
-                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                  currentPage === numberOfPages - 2 &&
-                  "bg-secondary border-secondary text-primary"
-                }`}
-              >
-                {currentPage === numberOfPages - 2 ? (
-                  <span className="sr-only">
-                    You are on the third-to-last page, page
-                  </span>
-                ) : (
-                  <span className="sr-only">
-                    Jump to the third-to-last page, page
-                  </span>
-                )}
-                {numberOfPages - 2}
-              </button>
-              <button
-                onClick={() => setCurrentPage(numberOfPages - 1)}
-                className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
-                  currentPage === numberOfPages - 1 &&
-                  "bg-secondary border-secondary text-primary"
-                }`}
-              >
-                {currentPage === numberOfPages - 1 ? (
-                  <span className="sr-only">
-                    You are on the second-to-last page, page
-                  </span>
-                ) : (
-                  <span className="sr-only">
-                    Jump to the second-to-last page, page
-                  </span>
-                )}
-                {numberOfPages - 1}
-              </button>
+              {displayRemainingPageButtons()}
               <button
                 onClick={() => setCurrentPage(numberOfPages)}
                 className={`border duration-150 ease-in-out hover:bg-secondary hover:border-secondary hover:text-primary inline-flex px-4 py-2 transition ${
