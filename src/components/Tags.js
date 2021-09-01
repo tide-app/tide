6aa8fc6f837c79af414a62074d473a3cb098a612
Hamir Mahal <hamirmahal@gmail.com>
2021-09-01 08:03:01 -0700

    fix #322 by fixing incorrect e2e-ids for tags

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -6,7 +6,7 @@ export default function Tags({ tags = [] }) {
     <>
       <div className="py-4 text-left mb-4">
         {tags.map((tag) => (
-          <Link data-e2e-id="SoundList-track-url" key={tag} to={`/tag/${tag}`}>
+          <Link data-e2e-id="Sound-tag-url" key={tag} to={`/tag/${tag}`}>
             <div
               key={tag}
               className="hover:bg-secondary hover:text-primary hover:opacity-100 opacity-50 transition duration-150 ease-in-out inline-block px-3 rounded mr-4 mb-4 border border-solid border-secondary"
