0335879c636b73644742b9dceccc64c711c32bb6
hamirmahal <hamirmahal@gmail.com>
2022-01-09 12:34:24 -0800

    fix formatting so we can compile, to fix #338

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import "./build.css";
 
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
-    dsn: "https://db77445a37ad4dcbaa33ca7f33dec571@o184269.ingest.sentry.io/5558822",
+    dsn:
+      "https://db77445a37ad4dcbaa33ca7f33dec571@o184269.ingest.sentry.io/5558822",
     autoSessionTracking: true,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 0.1,
